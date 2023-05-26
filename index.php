55656d2061a8e0af2fee04edf03378d3d14fce3b
M4dKiwi <sharnsutherlandwebdesigner@gmail.com>
2023-05-26 14:19:56 +1200

    Update index.php - bootstrap 5.3 CDN component
    Added Bootstrap 5.3 CDN component

--- index.php
+++ index.php
@@ -127,6 +127,9 @@ if ($menuObj->getActive() == $menuObj->getDefault() && $hideComponent)
 	<jdoc:include type="metas" />
 	<jdoc:include type="styles" />
 	<jdoc:include type="scripts" />
+	
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">	
+	
 </head>
 <body class="site-grid site <?php echo $pageclass . $hasSidebar; ?>">
 	<header class="grid-child container-header full-width header <?php echo $this->countModules('banner') ? 'has-banner' : ''; ?>">
