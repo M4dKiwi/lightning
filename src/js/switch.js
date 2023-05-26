65b03dc572205bfeb7b92da30f7480c7c46aab51
M4dKiwi <sharnsutherlandwebdesigner@gmail.com>
2023-05-26 15:24:28 +1200

    Update switch.js - added semicolons to remove parsing errors
    https://www.beautifyconverter.com/javascript-minifier.php
    was throwing up parsing errors , fixed by adding semicolins

--- src/js/switch.js
+++ src/js/switch.js
@@ -4,62 +4,73 @@
  */
 
 (() => {
-  // Theme states
-  const THEME = {
-    dark: 'is-dark',
-    light: 'is-light',
-    system: 'is-system',
-  }
+	// Theme states
+	const THEME = {
+		dark: 'is-dark',
+		light: 'is-light',
+		system: 'is-system',
+	};
 
-  // Method to change the class on the <html> element
-  const applyClass = (value) => {
-    html.classList.remove(THEME.dark, THEME.light, THEME.system)
-    html.classList.add(value)
-    html.setAttribute('data-bs-theme', value)
-  }
+	// Method to change the class on the <html> element
+	const applyClass = (value) => {
+		html.classList.remove(THEME.dark, THEME.light, THEME.system);
+		html.classList.add(value);
+		html.setAttribute('data-bs-theme', value);
+	};
 
-  // Variables
-  const html = document.documentElement
-  const matchMedia = window.matchMedia('(prefers-color-scheme:dark)')
-  const switcher = document.getElementById('color-scheme-switch')
+	// Variables
+	const html = document.documentElement;
+	const matchMedia = window.matchMedia('(prefers-color-scheme:dark)');
+	const switcher = document.getElementById('color-scheme-switch');
 
-  if (switcher) {
-    const colourScheme = matchMedia.matches ? THEME.dark : THEME.light
-    const storedTheme = localStorage.getItem('theme') !== null ? localStorage.getItem('theme') : THEME.system
+	if (switcher) {
+		const colourScheme = matchMedia.matches ? THEME.dark : THEME.light;
+		const storedTheme = localStorage.getItem('theme') !== null ? localStorage.getItem('theme') : THEME.system;
 
-    // Set the default input to be checked
-    if (storedTheme !== THEME.system) {
-      switcher.querySelector(`input[value="${storedTheme}"]`).checked = true
-      applyClass(storedTheme)
-    } else {
-      // Set default theme
-      switcher.querySelector(`input[value="${THEME.system}"]`).checked = true
-      matchMedia.matches ? applyClass(THEME.dark) : applyClass(THEME.light)
-    }
+		// Set the default input to be checked
+		if (storedTheme !== THEME.system) {
+			switcher.querySelector(`input[value="${storedTheme}"]`).checked = true;
+			applyClass(storedTheme);
+		} else {
+			// Set default theme
+			switcher.querySelector(`input[value="${THEME.system}"]`).checked = true;
+			matchMedia.matches ? applyClass(THEME.dark) : applyClass(THEME.light);
+		}
 
-    // Loop through all switcher inputs
-    switcher.querySelectorAll('input').forEach((input) => {
-      input.addEventListener('change', ({ target }) => {
-        if (target.value === THEME.dark || target.value === THEME.light) {
-          localStorage.setItem('theme', target.value)
-          applyClass(target.value)
-        } else {
-          localStorage.setItem('theme', THEME.system)
-          applyClass(colourScheme)
-        }
-      })
-    })
-  } else {
-    // Set default theme
-    matchMedia.matches ? applyClass(THEME.dark) : applyClass(THEME.light)
-  }
+		// Loop through all switcher inputs
+		switcher.querySelectorAll('input').forEach((input) => {
+			input.addEventListener('change', ({
+				target
+			}) => {
+				if (target.value === THEME.dark || target.value === THEME.light) {
+					localStorage.setItem('theme', target.value);
+					applyClass(target.value);
+				} else {
+					localStorage.setItem('theme', THEME.system);
+					applyClass(colourScheme);
+				}
+			});
+		});
+	} else {
+		// Set default theme
+		matchMedia.matches ? applyClass(THEME.dark) : applyClass(THEME.light);
+	}
 
-  // Listen for changes made to the system
-  matchMedia.addListener((event) => {
-    // If the switcher is disabled or enabled BUT set to the system preference, change the theme
-    // The theme will not change if it has manually been set by the switcher 
-    if (!switcher || (switcher && switcher.querySelector(`input[value="${THEME.system}"]`).checked)) {
-      return event.matches ? applyClass(THEME.dark) : applyClass(THEME.light)
-    }
-  })
-})()
+	// Listen for changes made to the system
+	matchMedia.addListener((event) => {
+		// If the switcher is disabled or enabled BUT set to the system preference, change the theme
+		// The theme will not change if it has manually been set by the switcher 
+		if (!switcher || (switcher && switcher.querySelector(`input[value="${THEME.system}"]`).checked)) {
+			return event.matches ? applyClass(THEME.dark) : applyClass(THEME.light);
+		}
+	});
+})();
+
+
+document.getElementById('color-scheme-switch').addEventListener('click', () => {
+	if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+		document.documentElement.setAttribute('data-bs-theme', 'light');
+	} else {
+		document.documentElement.setAttribute('data-bs-theme', 'dark');
+	}
+});
